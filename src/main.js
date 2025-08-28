fdc2ac282e30ea09409ee94f18f6b3bedc079a10
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-08-28 00:05:40 -0500

    Update icon loads with correct location
    Icon now displays and changes depending on the
    weather at searched location

--- src/main.js
+++ src/main.js
@@ -8,19 +8,25 @@ const input = document.querySelector(".text")
 
 const images = require.context('../icons', false, /\.(png|jpe?g|svg)$/);
 
+// creates img element for each icon
+function changeIcon(icon) {
 
-images.keys().forEach((filename) => {
-  const imgSrc = images(filename); // This gives the processed URL
-
-
-  // Example: add images to the page
-  const img = document.createElement("img");
-  img.src = imgSrc;
-  let length = filename.length
-  // gets rid of ./ in file name
-  img.classList.add(filename.slice(2,length))
-  document.body.appendChild(img);
-});
+    images.keys().forEach((filename) => {
+        const imgSrc = images(filename); // This gives the processed URL
+        const widget = document.querySelector('.widget')
+        let img = document.createElement("img");
+        let location = document.querySelector('.location')
+        img.src = imgSrc;
+        let length = filename.length
+        let reverse = length * -1
+        // gets rid of ./ in file name
+        var className = filename.slice(2,length).slice(reverse, length-6)
+        img.classList.add(className)
+        if (className == icon) {
+            location.after(img)
+        }
+    })
+}
 
 
 
@@ -38,6 +44,7 @@ async function getWeather(location, API_KEY) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${API_KEY}&contentType=json`, {mode: 'cors'})
     const data = await response.json()
     console.log(data)
+    const imgElement = document.querySelector('.icon')
 
     // Update widget info
     let position = document.querySelector(".location")
@@ -48,6 +55,9 @@ async function getWeather(location, API_KEY) {
 
     let description = document.querySelector(".description")
     description.textContent = data.description
+
+    changeIcon(data.currentConditions.icon)
+
 }
 
 
@@ -56,7 +66,4 @@ searchButton.addEventListener('click', () => {
     let location = document.querySelector(".text").value.toLowerCase()
     getWeather(location, API_KEY);
     localStorage.setItem("location", location)
-
-    let conditions = data.icon 
-    console.log(icon)
 })
